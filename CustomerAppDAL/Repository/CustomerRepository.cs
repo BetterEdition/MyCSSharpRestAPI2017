bc6d1f47f3faeabdc87d7282fd2587fb8ba813d0
Jesper Enemark <jesp6058@easv365.dk>
2017-09-24 12:33:24 +0200

    untracked files

---
+++ CustomerAppDAL/Repository/CustomerRepository.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CustomerAppDAL.Context;
+using System.Linq;
+using CustomerAppDAL.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CustomerAppDAL.Repository
+{
+    class CustomerRepository : ICustomerRepository
+    {
+
+        CustomerAppContext _context;
+
+        public CustomerRepository(CustomerAppContext context)
+        {
+            _context = context;
+        }
+        public Customer Create(Customer cust)
+        {
+            
+            _context.Customers.Add(cust);
+            
+            return cust;
+        }
+
+        public List<Customer> GetAll()
+        {
+            return _context.Customers.
+                Include(c => c.Addresses).
+                ToList();
+        }
+
+        public Customer Get(int Id)
+        {
+            return _context.Customers.FirstOrDefault(x => x.Id == Id);
+        }
+
+        public Customer Delete(int Id)
+        {
+            var cust = Get(Id);
+            _context.Customers.Remove(cust);
+           
+            return cust;
+        }
+    }
+}
